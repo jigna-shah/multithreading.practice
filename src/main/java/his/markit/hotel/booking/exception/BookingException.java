24533d97b5d438b9a4de9b009d77eaad92cb992e
jigna-shah <jigna.shah21@gmail.com>
2017-05-24 11:38:08 +0100

    changed

--- src/main/java/his/markit/hotel/booking/exception/BookingException.java
+++ src/main/java/his/markit/hotel/booking/exception/BookingException.java
@@ -3,7 +3,7 @@ package his.markit.hotel.booking.exception;
 public final class BookingException extends Exception {
 	
 	public BookingException(String message) {
-		super(message);
+		super(message); //added
 	}
 
 }
